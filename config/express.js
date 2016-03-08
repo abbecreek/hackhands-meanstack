3b701e13c96129b584139b0c993db448abe7dbf8
Sam <samesch@gmail.com>
2016-03-08 15:11:29 -0600

    finished local authentication section

--- config/express.js
+++ config/express.js
@@ -1,9 +1,35 @@
+var config = require('./config');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
 var express = require('express');
+
 module.exports = function() {
   var app = express();
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  // use this code before any route definitions
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+  app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'OurSuperSecretCookieSecret'
+  }));
+
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
+
   require('../app/routes/index.server.routes.js')(app);
+  require('../app/routes/users.server.routes.js')(app);
+
   app.use(express.static('./public'));
+
   return app;
 };
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var port = 1337;
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./config/config');
+var mongoose = require('./config/mongoose');
+var passport = require('./config/passport');
 var express = require('./config/express');
+
+var db = mongoose();
+var passport = passport();
 var app = express();
-app.listen(port);
+
+app.listen(config.port);
+
 module.exports = app;
-console.log('Server running at http://localhost:' + port);
+console.log(process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
